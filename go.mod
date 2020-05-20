00cc881227ca8c1cd16bddd8c63b61732fb8a983
Ammar Bandukwala <ammar@ammar.io>
2020-05-19 22:20:25 -0500

    Begin Server skeleton

---
+++ go.mod
@@ -0,0 +1,9 @@
+module cdr.dev/wsep
+
+go 1.14
+
+require (
+	github.com/creack/pty v1.1.11
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
+	nhooyr.io/websocket v1.8.6
+)
--- server.go
+++ server.go
@@ -1,10 +1,35 @@
 package wsep
 
-import "nhooyr.io/websocket"
+import (
+	"context"
+	"io/ioutil"
+
+	"cdr.dev/wsep/internal/proto"
+	"golang.org/x/xerrors"
+	"nhooyr.io/websocket"
+)
 
 // Serve runs the server-side of wsep.
 // The execer may be another wsep connection for chaining.
 // Use LocalExecer for local command execution.
-func Serve(c *websocket.Conn, execer Execer) error {
+func Serve(ctx context.Context, c *websocket.Conn, execer Execer) error {
+	var header proto.Header
+	for {
+		typ, reader, err := c.Reader(ctx)
+		if err != nil {
+			return xerrors.Errorf("get reader: %w", err)
+		}
+		switch typ {
+		case websocket.MessageText:
+			// Header
 
+			// Allocate, because we have to read it twice.
+			byt, err := ioutil.ReadAll(reader)
+			if err != nil {
+				return xerrors.Errorf("read header: %w", err)
+			}
+		case websocket.MessageBinary:
+			// Body
+		}
+	}
 }
