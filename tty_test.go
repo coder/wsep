f917a76baec38f438264bacfe8b29baa3ce158e2
Asher <ash@coder.com>
2022-09-26 12:55:55 -0500

    Consolidate test scaffolding into helpers
    I think this helps make the tests a bit more concise.

--- tty_test.go
+++ tty_test.go
@@ -3,7 +3,8 @@ package wsep
 import (
 	"bufio"
 	"context"
-	"io/ioutil"
+	"fmt"
+	"math/rand"
 	"strings"
 	"sync"
 	"testing"
@@ -17,335 +18,180 @@ import (
 func TestTTY(t *testing.T) {
 	t.Parallel()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	// Run some output in a new session.
+	server := newServer(t)
+	ctx, command := newSession(t)
+	command.ID = "" // No ID so we do not start a reconnectable session.
+	process, _ := connect(ctx, t, command, server, nil)
+	expected := writeUnique(t, process)
+	assert.True(t, "find initial output", checkStdout(t, process, expected, []string{}))
 
-	wsepServer := NewServer()
-	defer wsepServer.Close()
-	defer assert.Equal(t, "no leaked sessions", 0, wsepServer.SessionCount())
-
-	ws, server := mockConn(ctx, t, wsepServer, nil)
-	defer ws.Close(websocket.StatusInternalError, "")
-	defer server.Close()
-
-	execer := RemoteExecer(ws)
-	testTTY(ctx, t, execer)
+	// Connect to the same session.  There should not be shared output since
+	// these end up being separate sessions due to the lack of an ID.
+	process, _ = connect(ctx, t, command, server, nil)
+	unexpected := expected
+	expected = writeUnique(t, process)
+	assert.True(t, "find new session output", checkStdout(t, process, expected, unexpected))
 }
 
-func testTTY(ctx context.Context, t *testing.T, e Execer) {
-	process, err := e.Start(ctx, Command{
+func TestReconnectTTY(t *testing.T) {
+	t.Run("DeprecatedServe", func(t *testing.T) {
+		// Do something in the first session.
+		ctx, command := newSession(t)
+		process, _ := connect(ctx, t, command, nil, nil)
+		expected := writeUnique(t, process)
+		assert.True(t, "find initial output", checkStdout(t, process, expected, []string{}))
+
+		// Connect to the same session.  Should see the same output.
+		process, _ = connect(ctx, t, command, nil, nil)
+		assert.True(t, "find reconnected output", checkStdout(t, process, expected, []string{}))
+	})
+
+	t.Run("NoScreen", func(t *testing.T) {
+		t.Setenv("PATH", "/bin")
+
+		// Run some output in a new session.
+		server := newServer(t)
+		ctx, command := newSession(t)
+		process, _ := connect(ctx, t, command, server, nil)
+		expected := writeUnique(t, process)
+		assert.True(t, "find initial output", checkStdout(t, process, expected, []string{}))
+
+		// Connect to the same session.  There should not be shared output since
+		// these end up being separate sessions due to the lack of screen.
+		process, _ = connect(ctx, t, command, server, nil)
+		unexpected := expected
+		expected = writeUnique(t, process)
+		assert.True(t, "find new session output", checkStdout(t, process, expected, unexpected))
+	})
+
+	t.Run("Regular", func(t *testing.T) {
+		t.Parallel()
+
+		// Run some output in a new session.
+		server := newServer(t)
+		ctx, command := newSession(t)
+		process, disconnect := connect(ctx, t, command, server, nil)
+		expected := writeUnique(t, process)
+		assert.True(t, "find initial output", checkStdout(t, process, expected, []string{}))
+
+		// Reconnect and sleep; the inactivity timeout should not trigger since we
+		// were not disconnected during the timeout.
+		disconnect()
+		process, disconnect = connect(ctx, t, command, server, nil)
+		time.Sleep(time.Second)
+		expected = append(expected, writeUnique(t, process)...)
+		assert.True(t, "find reconnected output", checkStdout(t, process, expected, []string{}))
+
+		// Make a simultaneously active connection.
+		process2, disconnect2 := connect(ctx, t, command, server, &Options{
+			// Divide the time to test that the heartbeat keeps it open through
+			// multiple intervals.
+			SessionTimeout: time.Second / 4,
+		})
+
+		// Disconnect the first connection and wait for inactivity.  The session
+		// should stay up because of the second connection.
+		disconnect()
+		time.Sleep(time.Second)
+		expected = append(expected, writeUnique(t, process2)...)
+		assert.True(t, "find second connection output", checkStdout(t, process2, expected, []string{}))
+
+		// Disconnect the second connection and wait for inactivity.  The next
+		// connection should start a new session so we should only see new output
+		// and not any output from the old session.
+		disconnect2()
+		time.Sleep(time.Second)
+		process, disconnect = connect(ctx, t, command, server, nil)
+		unexpected := expected
+		expected = writeUnique(t, process)
+		assert.True(t, "find new session output", checkStdout(t, process, expected, unexpected))
+	})
+
+	t.Run("Alternate", func(t *testing.T) {
+		t.Parallel()
+
+		// Run an application that enters the alternate screen.
+		server := newServer(t)
+		ctx, command := newSession(t)
+		process, disconnect := connect(ctx, t, command, server, nil)
+		write(t, process, "./ci/alt.sh")
+		assert.True(t, "find alt screen", checkStdout(t, process, []string{"./ci/alt.sh", "ALT SCREEN"}, []string{}))
+
+		// Reconnect; the application should redraw.  We should have only the
+		// application output and not the command that spawned the application.
+		disconnect()
+		process, disconnect = connect(ctx, t, command, server, nil)
+		assert.True(t, "find reconnected alt screen", checkStdout(t, process, []string{"ALT SCREEN"}, []string{"./ci/alt.sh"}))
+
+		// Exit the application and reconnect.  Should now be in a regular shell.
+		write(t, process, "q")
+		disconnect()
+		process, _ = connect(ctx, t, command, server, nil)
+		expected := writeUnique(t, process)
+		assert.True(t, "find shell output", checkStdout(t, process, expected, []string{}))
+	})
+}
+
+// newServer returns a new wsep server.
+func newServer(t *testing.T) *Server {
+	server := NewServer()
+	t.Cleanup(func() {
+		server.Close()
+		assert.Equal(t, "no leaked sessions", 0, server.SessionCount())
+	})
+	return server
+}
+
+// newSession returns a command for starting/attaching to a session with a
+// context for timing out.
+func newSession(t *testing.T) (context.Context, Command) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	t.Cleanup(cancel)
+
+	command := Command{
+		ID:      uuid.NewString(),
 		Command: "sh",
 		TTY:     true,
 		Stdin:   true,
 		Cols:    100,
 		Rows:    100,
 		Env:     []string{"TERM=xterm"},
-	})
-	assert.Success(t, "start sh", err)
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
+	}
 
-		stdout, err := ioutil.ReadAll(process.Stdout())
-		assert.Success(t, "read stdout", err)
-
-		t.Logf("bash tty stdout = %s", stdout)
-		prompt := string(stdout)
-		assert.True(t, `bash "$" or "#" prompt found`,
-			strings.HasSuffix(prompt, "$ ") || strings.HasSuffix(prompt, "# "))
-	}()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-
-		stderr, err := ioutil.ReadAll(process.Stderr())
-		assert.Success(t, "read stderr", err)
-		t.Logf("bash tty stderr = %s", stderr)
-		assert.True(t, "stderr is empty", len(stderr) == 0)
-	}()
-	time.Sleep(3 * time.Second)
-
-	process.Close()
-	wg.Wait()
+	return ctx, command
 }
 
-func TestReconnectTTY(t *testing.T) {
-	t.Run("DeprecatedServe", func(t *testing.T) {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
+// connect connects to a wsep server and runs the provided command.
+func connect(ctx context.Context, t *testing.T, command Command, wsepServer *Server, options *Options) (Process, func()) {
+	if options == nil {
+		options = &Options{SessionTimeout: time.Second}
+	}
+	ws, server := mockConn(ctx, t, wsepServer, options)
+	t.Cleanup(server.Close)
 
-		command := Command{
-			ID:      uuid.NewString(),
-			Command: "sh",
-			TTY:     true,
-			Stdin:   true,
-			Cols:    100,
-			Rows:    100,
-			Env:     []string{"TERM=xterm"},
-		}
+	process, err := RemoteExecer(ws).Start(ctx, command)
+	assert.Success(t, "start sh", err)
 
-		ws, server := mockConn(ctx, t, nil, nil)
-		defer server.Close()
-
-		process, err := RemoteExecer(ws).Start(ctx, command)
-		assert.Success(t, "start sh", err)
-
-		// Write some unique output.
-		echoCmd := "echo test:$((12+12))"
-		_, err = process.Stdin().Write([]byte(echoCmd + "\n"))
-		assert.Success(t, "write to stdin", err)
-		expected := []string{echoCmd, "test:24"}
-
-		assert.True(t, "find echo", checkStdout(t, process, expected, []string{}))
-
-		// Connect to the same session.
-		ws, server = mockConn(ctx, t, nil, nil)
-		defer server.Close()
-
-		process, err = RemoteExecer(ws).Start(ctx, command)
-		assert.Success(t, "start sh", err)
-
-		// Find the same output.
-		assert.True(t, "find echo", checkStdout(t, process, expected, []string{}))
-	})
-
-	t.Run("NoScreen", func(t *testing.T) {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-
-		t.Setenv("PATH", "/bin")
-
-		command := Command{
-			ID:      uuid.NewString(),
-			Command: "sh",
-			TTY:     true,
-			Stdin:   true,
-			Cols:    100,
-			Rows:    100,
-			Env:     []string{"TERM=xterm"},
-		}
-
-		wsepServer := NewServer()
-		defer wsepServer.Close()
-		defer assert.Equal(t, "no leaked sessions", 0, wsepServer.SessionCount())
-
-		ws, server := mockConn(ctx, t, wsepServer, nil)
-		defer server.Close()
-
-		process, err := RemoteExecer(ws).Start(ctx, command)
-		assert.Success(t, "start sh", err)
-
-		// Write some unique output.
-		echoCmd := "echo test:$((5+5))"
-		_, err = process.Stdin().Write([]byte(echoCmd + "\n"))
-		assert.Success(t, "write to stdin", err)
-		expected := []string{echoCmd, "test:10"}
-
-		assert.True(t, "find echo", checkStdout(t, process, expected, []string{}))
-
-		// Connect to the same session.
-		ws, server = mockConn(ctx, t, wsepServer, nil)
-		defer server.Close()
-
-		process, err = RemoteExecer(ws).Start(ctx, command)
-		assert.Success(t, "attach sh", err)
-
-		echoCmd = "echo test:$((6+6))"
-		_, err = process.Stdin().Write([]byte(echoCmd + "\r\n"))
-		assert.Success(t, "write to stdin", err)
-		unexpected := expected
-		expected = []string{"test:12"}
-
-		// No echo since it is a new process.
-		assert.True(t, "find echo", checkStdout(t, process, expected, unexpected))
-	})
-
-	t.Run("Regular", func(t *testing.T) {
-		t.Parallel()
-
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-
-		command := Command{
-			ID:      uuid.NewString(),
-			Command: "sh",
-			TTY:     true,
-			Stdin:   true,
-			Cols:    100,
-			Rows:    100,
-			Env:     []string{"TERM=xterm"},
-		}
-
-		wsepServer := NewServer()
-		defer wsepServer.Close()
-		defer assert.Equal(t, "no leaked sessions", 0, wsepServer.SessionCount())
-
-		ws, server := mockConn(ctx, t, wsepServer, &Options{
-			SessionTimeout: time.Second,
-		})
-		defer server.Close()
-
-		process, err := RemoteExecer(ws).Start(ctx, command)
-		assert.Success(t, "start sh", err)
-
-		// Write some unique output.
-		echoCmd := "echo test:$((1+1))"
-		_, err = process.Stdin().Write([]byte(echoCmd + "\n"))
-		assert.Success(t, "write to stdin", err)
-		expected := []string{echoCmd, "test:2"}
-
-		assert.True(t, "find echo", checkStdout(t, process, expected, []string{}))
-
-		// Disconnect.
+	return process, func() {
 		ws.Close(websocket.StatusNormalClosure, "disconnected")
 		server.Close()
+	}
+}
 
-		// Reconnect.
-		ws, server = mockConn(ctx, t, wsepServer, &Options{
-			SessionTimeout: time.Second,
-		})
-		defer server.Close()
+// writeUnique writes some unique output to the shell process and returns the
+// expected output.
+func writeUnique(t *testing.T, process Process) []string {
+	n := rand.Intn(100)
+	echoCmd := fmt.Sprintf("echo test:$((%d+%d))", n, n)
+	write(t, process, echoCmd)
+	return []string{echoCmd, fmt.Sprintf("test:%d", n+n)}
+}
 
-		process, err = RemoteExecer(ws).Start(ctx, command)
-		assert.Success(t, "attach sh", err)
-
-		// The inactivity timeout should not trigger since we are connected.
-		time.Sleep(time.Second)
-
-		echoCmd = "echo test:$((2+2))"
-		_, err = process.Stdin().Write([]byte(echoCmd + "\n"))
-		assert.Success(t, "write to stdin", err)
-		expected = append(expected, echoCmd, "test:4")
-
-		assert.True(t, "find echo", checkStdout(t, process, expected, []string{}))
-
-		// Make a simultaneously active connection.
-		ws2, server2 := mockConn(ctx, t, wsepServer, &Options{
-			// Divide the time to test that the heartbeat keeps it open through multiple
-			// intervals.
-			SessionTimeout: time.Second / 4,
-		})
-		defer server2.Close()
-
-		process, err = RemoteExecer(ws2).Start(ctx, command)
-		assert.Success(t, "attach sh", err)
-
-		// Disconnect the first connection.
-		ws.Close(websocket.StatusNormalClosure, "disconnected")
-		server.Close()
-
-		// Wait for inactivity.  It should still stay up because of the second
-		// connection.
-		time.Sleep(time.Second)
-
-		// This connection should still be up.
-		echoCmd = "echo test:$((3+3))"
-		_, err = process.Stdin().Write([]byte(echoCmd + "\n"))
-		assert.Success(t, "write to stdin", err)
-		expected = append(expected, echoCmd, "test:6")
-
-		assert.True(t, "find echo", checkStdout(t, process, expected, []string{}))
-
-		// Disconnect the second connection.
-		ws2.Close(websocket.StatusNormalClosure, "disconnected")
-		server2.Close()
-
-		// Wait for inactivity.
-		time.Sleep(time.Second)
-
-		// The next connection should start a new process.
-		ws, server = mockConn(ctx, t, wsepServer, &Options{
-			SessionTimeout: time.Second,
-		})
-		defer server.Close()
-
-		process, err = RemoteExecer(ws).Start(ctx, command)
-		assert.Success(t, "attach sh", err)
-
-		echoCmd = "echo test:$((4+4))"
-		_, err = process.Stdin().Write([]byte(echoCmd + "\r\n"))
-		assert.Success(t, "write to stdin", err)
-		unexpected := expected
-		expected = []string{"test:8"}
-
-		// This time no echo since it is a new process.
-		assert.True(t, "find echo", checkStdout(t, process, expected, unexpected))
-	})
-
-	t.Run("Alternate", func(t *testing.T) {
-		t.Parallel()
-
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-
-		command := Command{
-			ID:      uuid.NewString(),
-			Command: "sh",
-			TTY:     true,
-			Stdin:   true,
-			Cols:    100,
-			Rows:    100,
-			Env:     []string{"TERM=xterm"},
-		}
-
-		wsepServer := NewServer()
-		defer wsepServer.Close()
-		defer assert.Equal(t, "no leaked sessions", 0, wsepServer.SessionCount())
-
-		ws, server := mockConn(ctx, t, wsepServer, &Options{
-			SessionTimeout: time.Second,
-		})
-		defer server.Close()
-
-		process, err := RemoteExecer(ws).Start(ctx, command)
-		assert.Success(t, "attach sh", err)
-
-		// Run an application that enters the alternate screen.
-		_, err = process.Stdin().Write([]byte("./ci/alt.sh\n"))
-		assert.Success(t, "write to stdin", err)
-
-		assert.True(t, "find output", checkStdout(t, process, []string{"./ci/alt.sh", "ALT SCREEN"}, []string{}))
-
-		// Disconnect.
-		ws.Close(websocket.StatusNormalClosure, "disconnected")
-		server.Close()
-
-		// Reconnect; the application should redraw.
-		ws, server = mockConn(ctx, t, wsepServer, &Options{
-			SessionTimeout: time.Second,
-		})
-		defer server.Close()
-
-		process, err = RemoteExecer(ws).Start(ctx, command)
-		assert.Success(t, "attach sh", err)
-
-		// Should have only the application output.
-		assert.True(t, "find output", checkStdout(t, process, []string{"ALT SCREEN"}, []string{"./ci/alt.sh"}))
-
-		// Exit the application.
-		_, err = process.Stdin().Write([]byte("q"))
-		assert.Success(t, "write to stdin", err)
-
-		// Disconnect.
-		ws.Close(websocket.StatusNormalClosure, "disconnected")
-		server.Close()
-
-		// Reconnect.
-		ws, server = mockConn(ctx, t, wsepServer, &Options{
-			SessionTimeout: time.Second,
-		})
-		defer server.Close()
-
-		process, err = RemoteExecer(ws).Start(ctx, command)
-		assert.Success(t, "attach sh", err)
-
-		echoCmd := "echo test:$((5+5))"
-		_, err = process.Stdin().Write([]byte(echoCmd + "\r\n"))
-		assert.Success(t, "write to stdin", err)
-
-		assert.True(t, "find output", checkStdout(t, process, []string{echoCmd, "test:10"}, []string{}))
-	})
+// write writes the provided input followed by a newline to the shell process.
+func write(t *testing.T, process Process, input string) {
+	_, err := process.Stdin().Write([]byte(input + "\n"))
+	assert.Success(t, "write to stdin", err)
 }
 
 // checkStdout ensures that expected is in the stdout in the specified order.
